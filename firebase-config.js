7322aedc3e647e946c4578d7c887300dc8f6e157
lilysky17 <t7964149@gmail.com>
2025-06-17 21:32:11 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJqHgc9aYq6yEe0TMT7FE0FAUFLVi6HFg",
+  authDomain: "secret-chat-d58c2.firebaseapp.com",
+  projectId: "secret-chat-d58c2",
+  storageBucket: "secret-chat-d58c2.appspot.com",
+  messagingSenderId: "48865399285",
+  appId: "1:48865399285:web:da6dd1e1bfc5d684a81d7d",
+  measurementId: "G-GBDBP57R08"
+};
